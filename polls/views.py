6d12ae4773f0c914aee57749e02f7e81cfaba1b0
jsonwamuyu <jysonmuchiri@gmail.com>
2025-05-21 17:12:38 +0300

    Relocate polls folder in root direstory

---
+++ polls/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello world, You are at the index of poll page.")
+
+def login(request):
+    return HttpResponse("Login page")
