ab6fed59136826f5072cebcb9ad822482228c97f
jsonwamuyu <jysonmuchiri@gmail.com>
2025-05-24 08:09:20 +0300

    Add polls view logic

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from polls.models import Question
+
+
 def index(request):
-    return HttpResponse("Hello world, You are at the index of poll page.")
+    """Display latest 5 polls questions by publication date"""
+    latest_question_list = Question.objects.order_by("pub_date")
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
-def login(request):
-    return HttpResponse("Login page")
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id )
 
-def signup(request):
-    return HttpResponse('Create your account.g')
+def results(request, question_id):
+    response = "You are looking at results for question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = "You are voting on question %s"
+    return HttpResponse(response % question_id)
+
