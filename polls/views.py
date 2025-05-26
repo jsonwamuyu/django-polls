9112573e7ad0c19dd0357d12432a9a9610246ffa
jsonwamuyu <jysonmuchiri@gmail.com>
2025-05-26 11:53:42 +0300

    Add templates

--- polls/views.py
+++ polls/views.py
@@ -2,13 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from polls.models import Question
-
+from django.template import loader
 
 def index(request):
     """Display latest 5 polls questions by publication date"""
-    latest_question_list = Question.objects.order_by("pub_date")
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context  = {"latest_question_list":latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You are looking at question %s." % question_id )
