666f72e45aab763af2d1bf16b946aec15561ffd9
jsonwamuyu <jysonmuchiri@gmail.com>
2025-05-28 13:38:26 +0300

    Install djangorestframework

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "polls.apps.PollsConfig"
+    "polls.apps.PollsConfig",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
